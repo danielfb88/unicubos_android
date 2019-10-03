7cd7be68575b7a62a0cf67ef9ae198e9466c2f8e
Daniel Bonfim <daniel.bonfim@cubos.io>
2019-10-03 20:35:50 -0300

    inserindo imagem da web com a lib picasso

--- app/src/main/java/com/learning/unicuboslab/MainActivity.kt
+++ app/src/main/java/com/learning/unicuboslab/MainActivity.kt
@@ -3,12 +3,18 @@ package com.learning.unicuboslab
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Picasso
+            .get()
+            .load("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRepETJZr0ZHtIQvwwvxztMDnYWOoSl4ZMDPhQIDy8tsTt2r4Yq8w")
+            .into(imageView)
     }
 
     fun toastTest(text:String)=
