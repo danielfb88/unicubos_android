02728d3016d12f9b7218fc2de6df182266ffdad7
Daniel Bonfim <daniel.bonfim@cubos.io>
2019-10-03 20:09:47 -0300

    testando Toast: popup informativo

--- app/src/main/java/com/learning/unicuboslab/MainActivity.kt
+++ app/src/main/java/com/learning/unicuboslab/MainActivity.kt
@@ -2,11 +2,40 @@ package com.learning.unicuboslab
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun toastTest(text:String)=
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
+
+    override fun onRestart() {
+        super.onRestart()
+        toastTest(("RESTART TESTE"))
+    }
+
+    override fun onStart() {
+        super.onStart()
+        toastTest(("START TESTE"))
+    }
+
+    override fun onStop() {
+        super.onStop()
+        toastTest(("STOP TESTE"))
+    }
+
+    override fun onResume() {
+        super.onResume()
+        toastTest(("RESUME TESTE"))
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        toastTest(("DESTROY TESTE"))
     }
 }
