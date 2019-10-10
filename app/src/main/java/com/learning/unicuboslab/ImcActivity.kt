9b59f1787b9cb7f12f67000193e43e4eb7cca6ce
Daniel Bonfim <daniel.bonfim@cubos.io>
2019-10-10 20:46:20 -0300

    Relative layout

---
+++ app/src/main/java/com/learning/unicuboslab/ImcActivity.kt
@@ -0,0 +1,12 @@
+package com.learning.unicuboslab
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ImcActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_imc)
+    }
+}
