c98d39f4410ecb9d0a69c732e02442c6e9257cb2
Daniel Bonfim <daniel.bonfim@cubos.io>
2019-10-24 18:40:59 -0300

    wip

--- app/src/main/java/com/learning/unicuboslab/MainActivity.kt
+++ app/src/main/java/com/learning/unicuboslab/MainActivity.kt
@@ -3,7 +3,9 @@ package com.learning.unicuboslab
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.core.widget.doOnTextChanged
 import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_imc.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +13,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_imc)
+        initListeners()
         /*Picasso
             .get()
             .load("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRepETJZr0ZHtIQvwwvxztMDnYWOoSl4ZMDPhQIDy8tsTt2r4Yq8w")
@@ -18,6 +21,28 @@ class MainActivity : AppCompatActivity() {
 
          */
     }
+
+    private fun initListeners() {
+        calculateBTN?.setOnClickListener {
+            getValues()
+        }
+    }
+
+    private fun getValues() {
+        val altura:Float = heightEDTX?.text?.toString()?.toFloat() ?: 1f
+        val peso:Float = wightEDTX?.text?.toString()?.toFloat() ?: 0f
+        val resultado = calcIMC(altura, peso)
+
+        val title = when(resultado) {
+            in 0f..19f -> "Abaixo do peso"
+            in 19f..25f -> "Peso ideal"
+            else -> "Sobrepeso"
+        }
+
+        titleTX?.text = (resultado.toString() + " - " + title)
+    }
+
+    private fun calcIMC(altura: Float, peso: Float) = peso / (altura * altura)
 
     fun toastTest(text:String)=
         Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
